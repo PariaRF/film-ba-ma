4d2e6dac3157a38933d005cc314a7e810982ffb8
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-03-07 10:51:27 +0330

    install react icone and use it in header

--- src/component/layout/header/index.jsx
+++ src/component/layout/header/index.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../asset/logo.svg";
 import { Link } from "react-router-dom";
 import "./index.css";
 import DropDownMenu from "../../dropdown-menu";
+// import { BiSearchAlt2, BiPlusMedical, BiX } from 'react-icons/fa';
+import {  BiSearchAlt2, BiPlusMedical, BiX } from "react-icons/bi";
 
 
 const Header = () => {
+
+    const [click, setClick] = useState(true);
+
+    const handleClick = () => {
+        setClick(!click);
+    }
+
+
     return (
         <>
             <div className="header row">
@@ -16,7 +26,15 @@ const Header = () => {
                     <DropDownMenu />
                 </div>
                 <div className="col-md-6 right-side">
-                    
+                    <BiPlusMedical className="icon" />
+                   <p className="lang">EN</p>
+                    <Link className="link">Login</Link>
+                    <Link className="link">Join TMDB</Link>
+                    <span onClick={handleClick}>
+                        {
+                            click ? <BiSearchAlt2 className="icon-search" /> : <BiX className="iconBix" />
+                        }
+                    </span>
                 </div>
             </div>
         </>
