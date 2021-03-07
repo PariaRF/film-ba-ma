adf8fd797c851caf6fcaaa989be1c37766a7be65
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-03-07 14:15:11 +0330

    add folder and rename file

---
+++ src/component/pages/pages-of-menu/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pages = () => {
+    return (
+        <div>
+            <h1 style={{ margin: "300px 450px" }}>Welocme to pages</h1>
+        </div>
+    )
+}
+
+export default Pages;
