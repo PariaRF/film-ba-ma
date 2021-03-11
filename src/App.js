596b739b68bc80d8481b419a37b3f0e02805f799
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-03-11 12:35:25 +0330

    change some attribute header,footer,mainoage

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Pages from "./component/pages/pages-of-menu";
 import MainPage from './component/pages/main-page';
 import Footer from './component/layout/footer';
+import DetailPopularMovie from './component/pages/detail-popular-movie';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <MainPage />
-      <Footer />
+      {/* <MainPage /> */}
       <Switch>
-        <Route path="/movie/popular" component={Pages} />
-        <Route path="/movie/new-plaing" component={Pages} />
-        <Route path="/movie/upcoming" component={Pages} />
-        <Route path="/movie/top-rated" component={Pages} />
-        <Route path="/tv/popular" component={Pages} />
-        <Route path="/tv/airing-today" component={Pages} />
-        <Route path="/tv/on-tv" component={Pages} />
-        <Route path="/tv/top-rated" component={Pages} />
-        <Route path="/people/popular-people" component={Pages} />
-        <Route path="/more/discussion" component={Pages} />
-        <Route path="/more/leaderboard" component={Pages} />
-        <Route path="/more/support" component={Pages} />
-        <Route path="/more/api" component={Pages} />
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/movie/popular" component={Pages} />
+        <Route exact path="/movie/:id" component={DetailPopularMovie} />
+        <Route exact path="/movie/new-plaing" component={Pages} />
+        <Route exact path="/movie/upcoming" component={Pages} />
+        <Route exact path="/movie/top-rated" component={Pages} />
+        <Route exact path="/tv/popular" component={Pages} />
+        <Route exact path="/tv/airing-today" component={Pages} />
+        <Route exact path="/tv/on-tv" component={Pages} />
+        <Route exact path="/tv/top-rated" component={Pages} />
+        <Route exact path="/people/popular-people" component={Pages} />
+        <Route exact path="/more/discussion" component={Pages} />
+        <Route exact path="/more/leaderboard" component={Pages} />
+        <Route exact path="/more/support" component={Pages} />
+        <Route exact path="/more/api" component={Pages} />
       </Switch>
+      <Footer />
+
     </BrowserRouter>
    );
 }
