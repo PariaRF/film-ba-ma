2cca85bc3a35c8a8d5fca3a983e3c5c4be83c19f
paria rezaee fard <paria_rezaee4224@yahoo.com>
2021-03-11 12:37:26 +0330

    change something

--- src/component/pages/main-page/popular-box/index.jsx
+++ src/component/pages/main-page/popular-box/index.jsx
@@ -1,26 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // import axios from "axios";
 import "./index.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getPopularAction } from "../../../../store/actions/getPopularAction";
 import { getFullUrlImageAction } from "../../../../store/actions/getFullUrlImageAction";
-import { getPopularReducer } from "../../../../store/reducers/getPopularReducer";
+import {getPopularDetailAction} from "../../../../store/actions/getPopularDetailAction";
+// import { getPopularReducer } from "../../../../store/reducers/getPopularReducer";
+import { BiListUl } from "react-icons/bi";
+import { AiFillHeart, AiFillStar } from "react-icons/ai";
+import { BsBookmarkFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const PopularBox = () => {
+    const [showListOption, setShowListOption] = useState(false);
+    
+
     const getPopularMovie = useSelector(state => state.popularState.getPopular.results);
     const getImageUrl = useSelector(state => state.fullUrlImage.getFullUrlImage.images);
+    const getPopularDetail = useSelector(state => state.popularDetailState.getPopularDetail.id);
 
     const dispatch = useDispatch();
 
     useEffect(async () => {
         await dispatch(await getPopularAction(dispatch))
         await dispatch(await getFullUrlImageAction(dispatch))
+        await dispatch(await getPopularDetailAction(dispatch))
     }, []);
+
+    const showOption = (id) => {
+        console.log(showListOption);
+        setShowListOption(!showListOption);
+        if (showListOption) {
+            return (
+                <div className="list-option">
+                    <p>
+                        <BiListUl />
+                        Add to list
+                    </p>
+                    <hr />
+                    <p>
+                        <AiFillHeart />
+                        Favorite
+                    </p>
+                    <hr />
+                    <p>
+                        <BsBookmarkFill />
+                        Watchlist
+                    </p>
+                    <hr />
+                    <p>
+                        <AiFillStar />
+                        Your rating
+                    </p>
+                </div>
+            )
+        }
+    }
 
 
     return (
         <>
-            {/* <div className="container-fluid"> */}
+
             <div className="row ">
                 <div className="col-md-12">
                     <div className="header-col">
@@ -38,26 +78,27 @@ const PopularBox = () => {
                     <div className="row">
                         <div className="col-md-12">
                             <div className="popular-box scollor">
-                                {/* <div className="card-box"> */}
-
                                 {
                                     getImageUrl && getPopularMovie && getPopularMovie.map((item, index) => {
                                         return (
-                                            <div className="card-popular">
-                                                <img src={`${getImageUrl.base_url}${getImageUrl.poster_sizes[0]}${item.poster_path}`} key={index} className="box-pic" />
-                                                <h4 className="text-popular">{item.title}</h4>
+                                            <div className="card-popular" key={index}>
+                                                <span className="option" onClick={showOption}>
+                                                    <p className="option-text">...</p>
+                                                </span>
+                                                <Link to={`/movie/${item.id}`} className="link-movie">
+                                                    <img src={`${getImageUrl.base_url}${getImageUrl.poster_sizes[0]}${item.poster_path}`} key={index} className="box-pic" />
+                                                    <h4 className="text-popular">{item.title}</h4>
+                                                </Link>
+                                                <p className="date">{item.release_date}</p>
                                             </div>
                                         )
                                     })
                                 }
-
-                                {/* </div> */}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            {/* </div> */}
 
         </>
     )
